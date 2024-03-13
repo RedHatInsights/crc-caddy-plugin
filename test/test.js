0f2d79a416fd3e317f675d87971f45c847c0abd8
Matt Knop <mknop@redhat.com>
2024-03-13 14:22:37 -0600

    adding integration testing

---
+++ test/test.js
@@ -0,0 +1,24 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let chaiJSON = require('chai-json');
+var expect = chai.expect;
+let should = chai.should();
+var assert = chai.assert;
+
+
+chai.use(chaiHttp);
+chai.use(chaiJSON);
+
+const url = 'http://localhost:9080';
+
+// Test / route
+describe('/GET /',() => {
+    it('should return 200', (done) => {
+        chai.request(url)
+            .get('/')
+            .end((err,res) => {
+                res.should.have.status(200);
+            done();
+        });
+    });
+});
